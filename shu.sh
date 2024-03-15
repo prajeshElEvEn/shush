54bb9b88826106be4cbd8b2fdbeba3fafac726ed
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-03-16 01:52:21 +0530

    added rm git files in shu

--- shu.sh
+++ shu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 show_usage() {
-    echo "Usage: $0 [-r | -c | -h]"
+    echo "Usage: $0 [-r | -c | -g | -h]"
     echo "Options:"
     echo "  -r   Build and run the code"
     echo "  -c   Clean up the build"
+    echo "  -g   Removes git dirs/files. Use it before pushing code to your github (only for the first time)"
     echo "  -h   Show available commands and their descriptions"
 }
 
@@ -41,6 +42,13 @@ case "$1" in
             echo "> build not found"
         fi
         ;;
+    -g)
+        if [ -d ".git"]; then
+            rm -rf .git
+        if [ -d ".github"]; then
+            rm -rf .github
+        if [ -f "LICENSE"]; then
+            rm -rf LICENSE
     *)
         echo "Error: Invalid argument"
         show_usage
